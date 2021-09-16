7efb4ff976713ae338a19512ea35c6c8f72387bd
Darrell Tunnell <darrell.tunnell@googlemail.com>
2021-09-16 14:13:36 +0100

    Add support for linux paths

---
+++ src/DotNet.Cabinet/OperatingSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DotNet.Cabinets
+{
+    internal static class OperatingSystem
+    {
+        public static bool IsWindows() =>
+            RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+        public static bool IsMacOS() =>
+            RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+
+        public static bool IsLinux() =>
+            RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+    }
+
+
+}
